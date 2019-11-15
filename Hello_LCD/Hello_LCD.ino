9c338e20ec888cf09382e25c1a729368e7c4c499
John A Kammauff <jkammau97@ccs.local>
2019-11-15 12:00:27 -0500

    cleaned up code; added some comments

--- Hello_LCD/Hello_LCD.ino
+++ Hello_LCD/Hello_LCD.ino
@@ -7,7 +7,7 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12); // Those are the pins for RS, EN, DB4, D
 void setup() {
   // put your setup code here, to run once:
 
-  lcd.begin(16, 2);
+  lcd.begin(16, 2); //Initializes the lcd
   lcd.setCursor(0,0); //sets pos to print to, 0,0 is the very top left. (Column[0-15], row[0-1])
   lcd.print("hello, world!");
 }
@@ -16,6 +16,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   lcd.setCursor(0,1);
   lcd.print(timeSec);
-  delay(1000);
-  timeSec++;
+  delay(1000); //delay in milliseconds; 1000 ms = 1 s
+  timeSec++; //increments it by 1
 }
